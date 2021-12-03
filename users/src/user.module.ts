4362d77bee2c0b2f8181c921190257d2b75be0a5
Alexander Hunter <icehuntmen@gmail.com>
2021-12-03 12:54:26 +0300

    Changes up

--- users/src/user.module.ts
+++ users/src/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './users/user.controller';
-import { UserService } from './users/user.service';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
 import { RolesController } from './roles/roles.controller';
 import { RolesService } from './roles/roles.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfigService } from './providers/jwt.servises';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoConfigService } from './providers/mongo.service';
 
 @Module({
   imports: [
@@ -11,6 +15,22 @@ import { RolesService } from './roles/roles.service';
       isGlobal: true,
       envFilePath: `.env`,
     }),
+    JwtModule.registerAsync({
+      useClass: JwtConfigService,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongoConfigService,
+    }),
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: 'UserSchema', // TODO: Сделать объектом
+      },
+      {
+        name: 'Roles',
+        schema: 'RolesSchema', // TODO: Сделать объектом
+      },
+    ]),
   ],
   controllers: [UserController, RolesController],
   providers: [UserService, RolesService],
