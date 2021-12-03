97d8d031d42d1f5d392065139cbceb7f879ed55f
Alexander Hunter <icehuntmen@gmail.com>
2021-12-03 12:51:17 +0300

    User Change structures fix.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:16
+ENV NODE_ENV=production
 RUN mkdir -p /var/www/users
 WORKDIR /var/www/users
 ADD . /var/www/users
--- src/user.module.ts
+++ src/user.module.ts
@@ -4,6 +4,10 @@ import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
 import { RolesController } from './roles/roles.controller';
 import { RolesService } from './roles/roles.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfigService } from './providers/jwt.servises';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoConfigService } from './providers/mongo.service';
 
 @Module({
   imports: [
@@ -11,6 +15,22 @@ import { RolesService } from './roles/roles.service';
       isGlobal: true,
       envFilePath: `.env`,
     }),
+    JwtModule.registerAsync({
+      useClass: JwtConfigService,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongoConfigService,
+    }),
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: 'UserSchema', // TODO: Сделать объектом
+      },
+      {
+        name: 'Roles',
+        schema: 'RolesSchema', // TODO: Сделать объектом
+      },
+    ]),
   ],
   controllers: [UserController, RolesController],
   providers: [UserService, RolesService],
