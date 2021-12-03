8a1fce8a7a1d78444fb4a79b208fcdcfaa6a63d5
Alexander Hunter <icehuntmen@gmail.com>
2021-12-03 12:12:38 +0300

    Change structures fix.

--- src/user.module.ts
+++ src/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './users/user.controller';
-import { UserService } from './users/user.service';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
 import { RolesController } from './roles/roles.controller';
 import { RolesService } from './roles/roles.service';
